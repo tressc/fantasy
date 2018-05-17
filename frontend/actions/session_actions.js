6cda16362897bb5e259e1a67ff54fb221ee915e7
Colin <tresscd@gmail.com>
2018-05-17 15:50:10 -0400

    Add session actions and session api util

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -2,6 +2,8 @@ import * as APIUTil from '../util/session_api_util';
 
 export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
 export const LOGOUT_USER = 'LOGOUT_USER';
+export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS';
+export const REMOVE_ERRORS = 'REMOVE_ERRORS';
 
 const logoutUser = () => {
   return {
@@ -9,6 +11,36 @@ const logoutUser = () => {
   };
 };
 
+export const removeErrors = () => {
+  return {
+    type: REMOVE_ERRORS
+  };
+};
+
+// TODO: deconstruct user data from jbuilder
+
+const receiveCurrentUser = (user) => {
+  return {
+    type: RECEIVE_CURRENT_USER,
+    user
+  };
+};
+
+const receiveErrors = (errors) => {
+  return {
+    type: RECEIVE_SESSION_ERRORS,
+    errors
+  };
+};
+
 export const logout = () => (dispatch) => {
   return APIUTil.logout().then(() => dispatch(logoutUser()));
+};
+
+export const login = (user) => (dispatch) => {
+  return APIUTil.logIn(user).then(u => {
+    dispatch(receiveCurrentUser(u));
+  }, errors => {
+    dispatch(receiveErrors(errors.responseJSON));
+  });
 };
--- frontend/util/session_api_util.js
+++ frontend/util/session_api_util.js
@@ -1,6 +1,22 @@
-export const logOut = () => {
+export const signUp = (user) => {
+  return $.ajax({
+    url: 'api/users',
+    method: 'POST',
+    data: user
+  });
+};
+
+export const logIn = (user) => {
   return $.ajax({
     url: '/api/session',
+    method: 'POST',
+    data: user
+  });
+};
+
+export const logOut = () => {
+  return $.ajax(
+{    url: '/api/session',
     method: 'DELETE'
   });
 };
