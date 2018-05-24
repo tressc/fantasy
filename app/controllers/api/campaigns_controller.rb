f30767c0284b37a8492c992a4f3f4b2bddc7d9d8
Colin <tresscd@gmail.com>
2018-05-24 17:22:28 -0400

    Add content to controllers

--- app/controllers/api/campaigns_controller.rb
+++ app/controllers/api/campaigns_controller.rb
@@ -1,13 +1,39 @@
 class Api::CampaignsController < ApplicationController
   def create
+    @campaign = Campaign.new(campaign_params)
+    if @campaign.save
+      render :show
+    else
+      render json: @campaign.errors.full_messages, status: 403
+    end
   end
 
+  # TODO: params nesting may need to be reworked
+
   def show
+    @campaign = Campaign.find(params[:id])
   end
 
   def destroy
+    @campaign = Campaign.find(params[:id])
+    if current_user.id == @campaign.gm.id
+      @campaign.destroy
+    end
   end
 
   def update
+    @campaign = Campaign.find(params[:id])
+    @campaign.title = params[:campaign][:title]
+    @campaign.description = params[:campaign][:description]
+    if @campaign.save
+      render :show
+    else
+      render json: @campaign.errors.full_messages, status: 403
+    end
+  end
+
+  private
+  def campaign_params
+    params.require(:campaign).permit(:title, :description)
   end
 end
--- app/controllers/api/memberships_controller.rb
+++ app/controllers/api/memberships_controller.rb
@@ -1,7 +1,24 @@
 class Api::MembershipsController < ApplicationController
+
+  # TODO: may need to change some of this
+
   def create
+    @membership = Membership.new(membership_params)
+    if @membership.save
+      render :show
+    end
+  end
+
+  def show
   end
 
   def destroy
+    @membership = Membership.find(params[:id])
+    @membership.destroy
+  end
+
+  private
+  def membership_params
+    params.require(:membership).permit(:player_id, :campaign_id)
   end
 end
