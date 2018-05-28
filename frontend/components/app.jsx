faaa01154b1d11a1b3c2a620787efa6c03e8b20f
Colin <tresscd@gmail.com>
2018-05-28 18:07:17 -0400

    Add min heigh to nav bar

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -6,12 +6,14 @@ import LoginContainer from './session/login_form_container';
 import IndexContainer from './index/index_container';
 import NavContainer from './nav/nav_container';
 import ProfileContainer from './profile/profile_container';
+import ModalContainer from './modal/modal_container';
 
 // TODO: backticks? also protect all non-login/signup routes
 
 const App = () => {
   return (
     <div className='app'>
+      <ModalContainer />
       <Switch>
         <AuthRoute exact path='/login' component= { LoginContainer } />
         <AuthRoute exact path='/signup' component= { SignUpFormContainer} />
