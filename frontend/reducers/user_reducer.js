029064a0ec75bb9db62efdb1e0e9ec9f1f722880
Colin <tresscd@gmail.com>
2018-06-11 20:20:05 -0400

    Add new campain to user state

--- frontend/reducers/user_reducer.js
+++ frontend/reducers/user_reducer.js
@@ -1,5 +1,6 @@
 import { RECEIVE_USER } from '../actions/user_actions';
 import { RECEIVE_CURRENT_USER } from '../actions/session_actions';
+import { RECEIVE_CAMPAIGN } from '../actions/campaign_actions';
 import { merge } from 'lodash';
 
 const userReducer = (state = {}, action) => {
@@ -9,6 +10,11 @@ const userReducer = (state = {}, action) => {
       return merge({}, state, { [action.user.id]: action.user });
     case RECEIVE_CURRENT_USER:
       return merge({}, state, { [action.user.id]: action.user });
+    case RECEIVE_CAMPAIGN:
+      const gmId = action.campaign.gm_id;
+      const  newIds = state[gmId].run_campaign_ids.slice();
+      newIds.push(action.campaign.id);
+      return merge({}, state, {[gmId]: {run_campaign_ids: newIds}});
     default:
       return state;
   }
