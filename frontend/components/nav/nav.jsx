c1e87a8fb30af6be1027b69ba86340a1b3ba25db
Colin <tresscd@gmail.com>
2018-06-24 15:12:22 -0400

    Add mail dropdown to nav

--- frontend/components/nav/nav.jsx
+++ frontend/components/nav/nav.jsx
@@ -4,17 +4,57 @@ import { Link } from 'react-router-dom';
 class Nav extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      dropdown: 'closed-dropdown'
+    };
+    this.changeDropClass = this.changeDropClass.bind(this);
+  }
+
+  changeDropClass() {
+    if (this.state.dropdown === "closed-dropdown") {
+      this.setState({ dropdown: 'open-dropdown' });
+    } else {
+      this.setState({ dropdown: 'closed-dropdown' });
+    }
   }
 
   render() {
+    let dropdown;
+    if (this.props.pendings.length === 0) {
+      dropdown = <div>no mail at this time</div>;
+    } else {
+      this.props.pendings.map(membership => {
+        return (
+          <div key={ membership.id }>
+            <span>campaign name</span>
+            <span>gm name</span>
+            <button>approve</button>
+            <button>reject</button>
+          </div>
+        );
+      });
+    }
+
     const profileId = this.props.currentUser;
     return (
       <div className='nav-bar'>
         <div className='nav-left'>
-          <a href={`/#/users/${profileId}`}>profile</a>
-        </div>
-        <div className='nav-center'>
-          <a href='/#/'>home</a>
+          <div>
+            <a href='/#/'>home</a>
+          </div>
+          <div>
+            <a href={`/#/users/${profileId}`}>profile</a>
+          </div>
+          <div>
+            <button
+              onClick={ this.changeDropClass }
+              >
+              mail
+            </button>
+            <div className={ this.state.dropdown }>
+              { dropdown }
+            </div>
+          </div>
         </div>
         <div className='nav-right'>
           <button onClick={this.props.logout}>
--- frontend/components/nav/nav_container.js
+++ frontend/components/nav/nav_container.js
@@ -2,16 +2,28 @@ import React from 'react';
 import { connect } from 'react-redux';
 import Nav from './nav';
 import { logout } from '../../actions/session_actions';
+import {
+  approveMembership,
+  destroyMembership
+} from '../../actions/membership_actions';
 
 const msp = (state) => {
+  const currentUser = state.session.currentUser;
+  const pendingIds = state.entities.users[currentUser].pending_ids;
+  const pendings = pendingIds.map(id => {
+    return state.entities.memberships[id];
+  });
   return {
-    currentUser: state.session.currentUser
+    currentUser,
+    pendings
   };
 };
 
 const mdp = (dispatch) => {
   return {
-    logout: user => dispatch(logout(user))
+    logout: user => dispatch(logout(user)),
+    approveMembership: id => dispatch(approveMembership(id)),
+    destroyMembership: id => dispatch(destroyMembership(id))
   };
 };
 
