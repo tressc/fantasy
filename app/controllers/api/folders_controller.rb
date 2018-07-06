784ebb0571bb55dec3f6ce6b61a29e671d02eb79
Colin <tresscd@gmail.com>
2018-07-05 21:01:15 -0400

    Add folder page controllers

--- app/controllers/api/folders_controller.rb
+++ app/controllers/api/folders_controller.rb
@@ -1,13 +1,34 @@
 class Api::FoldersController < ApplicationController
   def create
+    @folder = Folder.new(folder_params)
+    if @folder.save
+      render :show
+    else
+      render json: @folder.errors.full_messages, status: 403
+    end
   end
 
   def update
+    @folder = Folder.find(params[:id])
+    # TODO does this work?
+    @folder.update_attributes(folder_params)
+    @folder.save
+    render :show
   end
 
   def destroy
+    @folder = Folder.find(params[:id])
+    @folder.destroy
+    render json: @folder
   end
 
   def show
+    @folder = Folder.find(params[:id])
+  end
+
+  private
+
+  def folder_params
+    params.require(:folder).permit(:campaign_id, :title, :status)
   end
 end
--- app/controllers/api/pages_controller.rb
+++ app/controllers/api/pages_controller.rb
@@ -1,13 +1,35 @@
 class Api::PagesController < ApplicationController
   def create
+    @page = Page.new(page_params)
+    if @page.save
+      render :show
+    else
+      render json: @page.errors.full_messages
+    end
   end
 
   def update
+    @page = Page.find(params[:id])
+    # TODO test if this works
+    @page.update_attributes(page_params)
+    # TODO is save necessary?
+    @page.save
+    render :show
   end
 
   def destroy
+    @page = Page.find(params[:id])
+    @page.destroy
+    render json: @page
   end
 
   def show
+    @page = Page.find(params[:id])
+  end
+
+  private
+
+  def page_params
+    params.require(:page).permit(:folder_id, :status, :title, :body)
   end
 end
