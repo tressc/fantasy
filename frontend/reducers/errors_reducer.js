fc422bab4323d069168ae0fa802c259126b453ff
Colin <tresscd@gmail.com>
2018-07-08 20:55:30 -0400

    Update errors reducer

--- frontend/reducers/errors_reducer.js
+++ frontend/reducers/errors_reducer.js
@@ -2,11 +2,15 @@ import { combineReducers } from 'redux';
 import sessionErrorsReducer from './session_errors_reducer';
 import campaignErrorsReducer from './campaign_errors_reducer';
 import membershipErrorsReducer from './membership_errors_reducer';
+import folderErrorsReducer from './folder_errors_reducer';
+import pageErrorsReducer from './page_errors_reducer';
 
 const errorsReducer = combineReducers({
   session: sessionErrorsReducer,
   campaign: campaignErrorsReducer,
-  membership: membershipErrorsReducer
+  membership: membershipErrorsReducer,
+  folder: folderErrorsReducer,
+  page: pageErrorsReducer
 });
 
 export default errorsReducer;
