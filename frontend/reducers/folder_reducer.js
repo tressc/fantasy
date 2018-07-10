1b66714c11b9c12af79e80fabbd03d20ad38d2a1
Colin <tresscd@gmail.com>
2018-07-10 17:38:06 -0400

    Add folder reducer

---
+++ frontend/reducers/folder_reducer.js
@@ -0,0 +1,12 @@
+import {
+  RECEIVE_FOLDER,
+  REMOVE_FOLDER,
+  UPDATE_FOLDER
+} from '../../actions/folder_actions';
+import { merge } from 'lodash';
+
+const folderReducer = (state = {}, action) => {
+
+};
+
+export default folderReducer;
